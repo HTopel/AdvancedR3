2e38597de6eb941eeecfca672ef90aec338561fc
Hande Topel <hande.topel@gmail.com>
2024-12-10 16:21:37 +0100

    descriptive statistics function

---
+++ R/functions.R
@@ -0,0 +1,21 @@
+#' Calculate descriptive statisticsof each metabolite
+#'
+#' @param data the lipidomics dataset
+#'
+#' @return a data.frame/tibble
+
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) |>
+        dplyr::summarise(dplyr::across(
+            value,
+            list(
+                mean = mean,
+                sd = sd
+            )
+        )) |>
+        dplyr::mutate(dplyr::across(
+            tidyselect::where(is.numeric),
+            ~ round(.x, digits = 1)
+        ))
+}
