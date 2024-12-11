4be7393e2083cc3cdde4e7ff34f30063bb3c9cfb
Hande Topel <hande.topel@gmail.com>
2024-12-11 10:03:52 +0100

    plot_distributions function

--- R/functions.R
+++ R/functions.R
@@ -19,3 +19,16 @@ descriptive_stats <- function(data) {
             ~ round(.x, digits = 1)
         ))
 }
+
+#' Plot for basic distribution of metabolite data
+#'
+#' @param data lipidomics dataset
+#'
+#' @return histogram of metabolites
+#'
+plot_distributions <- function(data) {
+    data |>
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
